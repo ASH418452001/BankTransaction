bae7de7e4aa5a0d3470cdeb4f51a68ea0f099ff6
ASH418452001 <138825174+ASH418452001@users.noreply.github.com>
2023-07-14 19:18:40 +0300

    New

--- BankTransaction/Program.cs
+++ BankTransaction/Program.cs
@@ -9,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(connectionString));
 builder.Services.AddControllers();
+//abod add
+// builder.Services.AddErrorHandling();
+//abod add
+// builder.Services.AddSignalR(cfg=> cfg.EnableDetailedErrors = true);
 builder.Services.AddJwt(builder.Configuration);
 builder.Services.AddAuthentication();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,9 +20,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingbankTransactionProfile));
 builder.Services.AddAutoMapper(typeof(MappingUserProfile));
+//abod add
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:3000");
+    });
+});
 
 
 var app = builder.Build();
+app.UseCors("CorsPolicy");
+
+
+app.UseRouting();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
@@ -30,10 +50,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapGet("/", async context =>
+//     {
+//         await context.Response.WriteAsync("BankTransaction");
+//     });
+//     endpoints.MapHub<MessageHub>("/messagehub");
+// });
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();    
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ index.js
@@ -0,0 +1,15 @@
+// // Require Express
+// const express = require('express');
+
+// // Create an instance of Express
+// const app = express();
+
+// // Define a route handler for the root URL
+// app.get('https://localhost:7012/api', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// // Start the server
+// app.listen(3000, () => {
+//   console.log('Server is listening on port 3000');
+// });
